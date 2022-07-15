5c9e3723db18200ccfb3e4ac0206a2083d529812
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-14 23:23:35 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,5 +22,11 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== Test 3: Seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
