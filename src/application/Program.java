8b1c3dde48ddc732b296214a3b496b1f0d73cea4
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-14 21:40:32 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,17 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
