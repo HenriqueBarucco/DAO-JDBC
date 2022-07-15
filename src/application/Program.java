4959b064d0a4b9a43f57d782f65be28de23fc10c
Henrique Barucco <henrique_barucco@hotmail.com>
2022-07-14 23:39:31 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -28,5 +29,10 @@ public class Program {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== Test 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
